74c4273ecc147e45a5e841629f4841d0656c7d79
unknown <multiplexor89@gmail.com>
2015-02-10 22:51:51 +0100

    Primera importación de Big P y animado con mecanim. (Aunque hay el problema de la orientacion del personaje). También se ha empezado a montar los scripts de la IA.

---
+++ Assets/Scripts/IAController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IAController : MonoBehaviour {
+
+
+	public float minimumDistanceSeePlayer = 30f;
+	public LayerMask layersToFindCollision;
+
+
+	private GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = (GameObject)GameObject.FindGameObjectWithTag("Player");
+	}
+
+	private bool canSeePlayer(){
+		Vector3 playerDirection = player.transform.position - transform.position;
+		if(playerDirection.magnitude<minimumDistanceSeePlayer){
+			RaycastHit hit;
+
+			if (Physics.Raycast(transform.position,playerDirection, out hit, minimumDistanceSeePlayer,layersToFindCollision))
+			{
+				Collider target = hit.collider; // What did I hit?
+				//Debug.Log(target.name);
+				if(target.tag == "Player"){ return true;}
+				else{ return false; }
+			}
+		}
+
+		return false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//canSeePlayer ();
+	}
+}
