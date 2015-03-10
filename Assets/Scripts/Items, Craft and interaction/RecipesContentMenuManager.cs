c1725ebef612036387daff6cf2e3da6fb2fe8e67
unknown <multiplexor89@gmail.com>
2015-03-10 01:49:54 +0100

    Añadido un ejemplo no funcional de como funciona el menu de crafting (para ejemplificar los iconos y los elementos que apareceran en el)

---
+++ Assets/Scripts/Items, Craft and interaction/RecipesContentMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecipesContentMenuManager : MonoBehaviour {
+
+	public GameObject[] recipes;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void activateRecipe(GameObject recipe){
+		foreach(GameObject recipeO in recipes){
+			recipeO.SetActive(false);
+		}
+		recipe.SetActive (true);
+	}
+}
