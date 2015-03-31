03ab0f4017de658bcb2e91134a039e3af850abdd
Gerardo Leon <leonluisgj@gmail.com>
2015-03-31 02:45:27 +0200

    Clonadas las animaciones capaces de recibir event triggers.

---
+++ Assets/Scripts/Player/AnimationPacker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationPacker : MonoBehaviour {
+
+	CharacterAttackController atkControl;
+
+	// Use this for initialization
+	//Clase temporal, porque BigP con la animacion debe ser el objeto base, actualmente es 
+	
+	void Start () {
+		atkControl = GetComponentInParent<CharacterAttackController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void enableHitbox() {
+	print("Hit Box!!!");
+	
+	}
+	
+	public void disalbeHitbox() {}
+	
+}
