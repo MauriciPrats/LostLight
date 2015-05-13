14a6e9bc3c778800a734ee4201e6b68c72e39773
Josep <Fry_hunter@bitbucket.org>
2015-05-13 22:06:07 +0200

    Siguiendo con la rata

--- Assets/Scripts/AI/Attacks/BurrowAttack.cs
+++ Assets/Scripts/AI/Attacks/BurrowAttack.cs
@@ -5,7 +5,10 @@ public class BurrowAttack : Attack {
 
 	public GameObject burrowParticles;
 
+	private GameObject parent;
 	private float direction = 0f;
+	private Animator iaAnimator;
+	private OutlineChanging outlineChanger;
 
 	public override void initialize ()
 	{
@@ -13,12 +16,12 @@ public class BurrowAttack : Attack {
 	}
 
 	public override void startAttack(){
-		bool isRight = gameObject.GetComponent<IAController>().getIsLookingRight();
+		/*bool isRight = gameObject.GetComponent<IAController>().getIsLookingRight();
 		if(isRight){
 			direction = 1f;
 		}else{
 			direction = -1f;
-		}
+		}*/
 		isFinished = false;
 		StartCoroutine ("burrow");
 	}
@@ -26,8 +29,19 @@ public class BurrowAttack : Attack {
 	private IEnumerator burrow() {
 
 		burrowParticles.SetActive (true);
+		Debug.Log ("Me hundo!");
 		yield return null;
 
 	}
+
+	public override void informParent(GameObject parentObject){
+		transform.parent = parentObject.transform;
+		transform.rotation = parentObject.transform.rotation;
+		transform.position = parentObject.GetComponent<Rigidbody>().worldCenterOfMass + (parentObject.transform.forward*parentObject.GetComponent<WalkOnMultiplePaths>().centerToExtremesDistance*1.5f);
+		parent = parentObject;
+		iaAnimator = parent.GetComponent<IAController> ().getIAAnimator ();
+		outlineChanger = parent.GetComponent<OutlineChanging> ();
+	}
+
 
 }
