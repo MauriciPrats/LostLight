40c6ca354251f0c0b639f915f19e5ecab360b818
Gerardo Leon <leonluisgj@gmail.com>
2015-05-18 17:52:35 +0200

    Primeros sonidos colocados

--- Assets/Scripts/Player/Attacks/ComboAttack.cs
+++ Assets/Scripts/Player/Attacks/ComboAttack.cs
@@ -13,10 +13,14 @@ public class ComboAttack : Attack, AnimationSubscriber {
 	private List<GameObject> enemiesHit;
 	private bool hasHitEnemy;
 
+	private BigPSoundEffectsControler soundManager;
+
 	public override void initialize() {
 		attackType = AttackType.Combo;
 		eventHandler = GameManager.playerAnimator.gameObject.GetComponent<AnimationEventBroadcast>();
 		eventHandler.subscribe(this);
+		
+		soundManager = GameManager.player.GetComponent<BigPSoundEffectsControler>();
 		stick = GameManager.player.GetComponent<PlayerController>().weapon.GetComponentInChildren<Collider>();
 		attackCollider = GameManager.player.GetComponent<PlayerController>().weapon.GetComponentInChildren<AttackCollider>();
 		weaponEffects = GameManager.player.GetComponent<PlayerController>().weapon.GetComponentInChildren<Xft.XWeaponTrail>();
@@ -65,6 +69,7 @@ public class ComboAttack : Attack, AnimationSubscriber {
 
 	public void enableHitbox() {
 		if(!isFinished){
+			soundManager.PlayWeaponSwing();
 			stick.enabled = true;
 			enemiesHit = new List<GameObject>(0);
 			hasHitEnemy = false;
