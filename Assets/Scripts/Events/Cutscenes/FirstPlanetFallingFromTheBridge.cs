d60a2f98eaf2766329d960093c3ba5f3c4ba2407
Josep <Fry_hunter@bitbucket.org>
2015-05-26 20:17:21 +0200

    Cutscene de la caida por el puente

---
+++ Assets/Scripts/Events/Cutscenes/FirstPlanetFallingFromTheBridge.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPlanetFallingFromTheBridge : Cutscene {
+
+	public GameObject bridge; 
+
+	public override void Initialize() {
+		identifyier = CutsceneIdentifyier.FirstPlanetFallingFromTheBridge;
+	}
+
+	public override void ActivateTrigger() {
+	}
+}
