4fbc7eaafb64e0d60fb15a0d34af35e6867c7124
unknown <multiplexor89@gmail.com>
2015-07-22 02:01:44 +0200

    Añadidas unas cuantas fuentes, Modificados los dialogos y los tutoriales para que utilizen la fuente Katana
    Se han modificado los dialogos para que cada personaje tenga su propio color.
    
    Añadidos algunos cambios preliminares para el mundo de la torre

---
+++ Assets/Scripts/Gravity and Planets/BreathingSpace.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreathingSpace : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider){
+		if(collider.gameObject.tag.Equals("Player")){
+			GameManager.player.GetComponent<PlayerController>().setCanDrownInSpace(false);
+		}
+	}
+	void OnTriggerExit(Collider collider){
+		if(collider.gameObject.tag.Equals("Player")){
+			GameManager.player.GetComponent<PlayerController>().setCanDrownInSpace(true);
+		}
+	}
+}
