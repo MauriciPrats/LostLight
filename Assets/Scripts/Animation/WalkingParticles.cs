e750b535e4f6c8d5ec9809e264deabe75069d5ea
Gerardo Leon <leonluisgj@gmail.com>
2015-07-24 02:21:22 +0200

    Particulas al andar y al saltar y aterrizar. Necesitan feedback de los artistas, usando mismo estilo que las del dash pero peque;as y mas rapidas.
    Falta
    Bug con las de salto que no se muestran en saltos perfectamente verticales.
    Cambiar las de aterrizaje para que su velocidad de expansion sea mayor simulando un golpe al suelo.

---
+++ Assets/Scripts/Animation/WalkingParticles.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkingParticles : MonoBehaviour, AnimationSubscriber {
+
+	ParticleSystem dust;
+
+	// Use this for initialization
+	void Start () {
+		AnimationEventBroadcast eventHandler = GameManager.playerAnimator.gameObject.GetComponent<AnimationEventBroadcast>();
+		eventHandler.subscribe(this);
+		
+		dust = this.GetComponent<ParticleSystem>();
+	}
+	
+	void AnimationSubscriber.handleEvent(string idEvent) {
+		if(idEvent == "step"){
+			dust.Play();
+		
+		}
+	}
+	
+	string AnimationSubscriber.subscriberName() {
+		return  "Walking";	
+	}
+	
+
+}
